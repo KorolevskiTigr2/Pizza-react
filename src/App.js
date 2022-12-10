82aba9848e06dd63c8a9c4e850b44f33d0c5c135
KorolevskiTigr2 <mega.medved87gmail.com>
2022-12-10 18:43:32 +0300

    now categories it's function

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function Header() {
     <div className="header">
        <div className="container">
         <div className="header_logo">
-         <img width="38" src="" alt="Pizza logo" />
+         <img width="38" src='../public/images/Los_pollos' alt="Pizza logo" />
           <div>
-            <h1>React pizza</h1>
-            <p>Вкусная пицца</p>
+            <h1>Los pollos Pizza</h1>
+            <p>Вкусная пицца </p>
           </div>
         </div>
         <div className="header_cart">
@@ -22,18 +22,37 @@ function Header() {
   );
 }
 
+function Categories(){
+  return(
+  <div className="categories">
+            <ul>
+              <li className='active'>Все</li>
+              <li>Мясные</li>
+              <li>Гриль</li>
+              <li>Вегетарианская</li>
+              <li>Острые</li>
+              <li>Закрытые</li>
+            </ul>
+          </div>
+ );
+}
+
 function App() {
   return (
-    <div className='wrapper'>
-      <Header />
-      <div className="container">
+    <div className='wrapper'>  
+      < Header />
+      <div className='content'>
         <div className='container'>
-          <div className='header_logo'>
-           
+          <div className="content_top">   
+             <Categories/>
+            <div className="sort">
+           <div className="sort_label">  
           </div>
+         </div>
         </div>
+       </div>   
       </div>
-    </div>
+   </div>
   );
 }
 
